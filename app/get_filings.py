6a755f37c86e32926f05d868c9c7ab5537537137
Mike Rossetti <s2t2@users.noreply.github.com>
2019-04-28 10:08:21 -0400

    Parse each filings line

--- app/get_filings.py
+++ app/get_filings.py
@@ -2,14 +2,17 @@ import requests
 
 def parse_line(line):
     """
+    assumes each line has the same five pipe-separated values
     @param line: a string like '1018724|AMAZON COM INC|10-K|2013-01-30|edgar/data/1018724/0001193125-13-028520.txt'
     """
+    values = line.split("|") #> ['1018724', 'AMAZON COM INC', '10-K', '2013-01-30', 'edgar/data/1018724/0001193125-13-028520.txt']
+
     return {
-        "company_id": "1018724",
-        "company_name": "AMAZON COM INC",
-        "form": "10-K",
-        "date": "2013-01-30",
-        "path": "edgar/data/1018724/0001193125-13-028520.txt"
+        "company_id": values[0],
+        "company_name": values[1],
+        "form": values[2],
+        "date": values[3],
+        "path": values[4]
     }
 
 if __name__ == "__main__":
@@ -36,4 +39,5 @@ if __name__ == "__main__":
     print(f"COMPANY {company_id} FILINGS ({len(company_lines)})...")
 
     for line in company_lines:
-        print(line)
+        filing = parse_line(line)
+        print(f"  + '{filing['company_name']}' {filing['path']}")
