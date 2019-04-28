819ebdca0fe7ea9816d1e69fb9b40c039b5d28ff
Mike Rossetti <s2t2@users.noreply.github.com>
2019-04-28 09:34:33 -0400

    Get master list of filings

---
+++ CREDITS.md
@@ -0,0 +1,7 @@
+# Credits, Notes, and Reference
+
+## SEC
+
+  + https://www.sec.gov/sitemap.shtml
+  + https://www.sec.gov/edgar.shtml
+  + https://www.sec.gov/Archives/edgar/data/1018724/0001193125-13-028520.txt
---
+++ get_filings.py
@@ -0,0 +1,7 @@
+
+year = "2013"
+quarter = "1"
+request_url = f"https://www.sec.gov/Archives/edgar/full-index/{year}/QTR{quarter}/master.idx"
+print("URL:", request_url) #> https://www.sec.gov/Archives/edgar/full-index/2013/QTR1/master.idx
+
+#company_id = "1018724" # AMAZON
